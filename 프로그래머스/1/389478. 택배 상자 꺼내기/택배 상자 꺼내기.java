e5969f83805e19fae70777f933f3927e3a012cfe
Eseop <74389493+KimEunSeop@users.noreply.github.com>
2025-03-27 15:43:03 +0900

    [level 1] Title: 택배 상자 꺼내기, Time: 0.04 ms, Memory: 84 MB -BaekjoonHub

---
+++ 프로그래머스/1/389478. 택배 상자 꺼내기/택배 상자 꺼내기.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int solution(int n, int w, int num) {
+       
+        int cnt = 0;
+        while(num <= n) {
+            num += (w - ((num-1) % w) -1) * 2 + 1;
+            cnt++;
+        }
+
+        int answer = cnt;
+        return answer;
+    }
+}
